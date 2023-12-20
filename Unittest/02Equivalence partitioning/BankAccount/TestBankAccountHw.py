8ff88c9e473316763bb164a4180808861851307b
PNYWT <punyawat.suwannatat@hotmail.com>
2023-12-20 22:25:24 +0700

    HW2 done

--- Unittest/02Equivalence partitioning/BankAccount/TestBankAccountHw.py
+++ Unittest/02Equivalence partitioning/BankAccount/TestBankAccountHw.py
@@ -16,12 +16,8 @@ class TestBankAccountHw(unittest.TestCase):
             self.account.withdraw(-99)
         self.assertEqual(1000, self.account.balance)
 
-    def test_withdraw_balance_greaterthanorequalto_amount_EP_A(self):
+    def test_withdraw_balance_greaterthanorequalto_amount_EP(self):
         self.account.withdraw(200)
-        self.assertEqual(800, self.account.balance)
-
-    def test_withdraw_balance_greaterthanorequalto_amount_EP_B(self):
-        self.account.withdraw(-200)
         self.assertEqual(800, self.account.balance)
 
     def test_withdraw_Balance_lessthan_amount_BVA(self):
@@ -34,12 +30,8 @@ class TestBankAccountHw(unittest.TestCase):
             self.account.withdraw(-1)
         self.assertEqual(1000, self.account.balance)
 
-    def test_withdraw_balance_greaterthanorequalto_amount_BVA_A(self):
+    def test_withdraw_balance_greaterthanorequalto_amount_BVA(self):
         self.account.withdraw(999)
-        self.assertEqual(1, self.account.balance)
-
-    def test_withdraw_balance_greaterthanorequalto_amount_BVA_B(self):
-        self.account.withdraw(-999)
         self.assertEqual(1, self.account.balance)
 
 if __name__ == "__main__":
