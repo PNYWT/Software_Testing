5dc00127b5d53440c622d43830d0dcc3954abe16
CallmeOnit <99156196+PNYWT@users.noreply.github.com>
2024-01-14 21:22:38 +0700

    update

---
+++ Unittest/04-05White_Box Condition Coverage/Homework/testTrigangle.py
@@ -0,0 +1,29 @@
+import unittest
+from trigangle import Trigangle
+
+class TestGeometry(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.trigangle = Trigangle()
+
+    def test_invalid_triangle(self):
+        self.assertEqual("Invalid", self.trigangle.get_triangle_type(-1,3,3)) #check
+        self.assertEqual("Invalid", self.trigangle.get_triangle_type(1,2,4)) #check
+        self.assertEqual("Invalid", self.trigangle.get_triangle_type(1,1,3)) #check
+
+    def test_Equilateral(self):
+        self.assertEqual("Equilateral", self.trigangle.get_triangle_type(3,3,3)) #check
+
+    def test_Isosceles(self):
+        self.assertEqual("Isosceles", self.trigangle.get_triangle_type(5,5,3)) #check
+        self.assertEqual("Isosceles", self.trigangle.get_triangle_type(3,5,5)) #check
+        self.assertEqual("Isosceles", self.trigangle.get_triangle_type(3,5,3)) #check
+
+    def test_Scalene(self):
+        self.assertEqual("Scalene", self.trigangle.get_triangle_type(3,4,7)) #check
+        self.assertEqual("Scalene", self.trigangle.get_triangle_type(5,6,7)) #check
+        self.assertEqual("Scalene", self.trigangle.get_triangle_type(5,7,5)) #check
+
+
+# if __name__ == "__main__":
+#     unittest.main()
---
+++ Unittest/04-05White_Box Condition Coverage/Homework/trigangle.py
@@ -0,0 +1,32 @@
+class Trigangle:
+    def __init__(self):
+        return
+
+    def get_triangle_type(self,a, b, c) -> str:
+        if (a <= 0) or (b <= 0) or (c <= 0):
+            return "Invalid"
+        type = 0
+        if a == b:
+            type = type + 1
+        if a == c:
+            type = type + 2
+        if b == c:
+            type = type + 3
+            
+        if type == 0:
+            if ((a + b) < c) or ((a + c) < b) or ((b + c) < a):
+                return "Invalid"
+            else:
+                return "Scalene"
+
+        if type > 3:
+            return "Equilateral"
+
+        if type == 1 and (a + b) > c :
+            return "Isosceles"
+        elif type == 2 and (a + c) > b :
+            return "Isosceles"
+        elif type == 3 and (b + c) > a :
+            return "Isosceles"
+
+        return "Invalid"
